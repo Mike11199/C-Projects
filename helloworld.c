73081981c9159310567354ad139becba668aad0a
Mike11199 <miwanek11199@gmail.com>
2021-10-14 22:32:00 -0700

    added files

--- helloworld.c
+++ helloworld.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include "cs50.h"
 
 int
 main (void)
 {
   printf ("Hello, world!  Installing the GCC compiler took forever!\n");
   printf ("Now I can print text in C.\n");
+
+  string answer = get_string("What's your name? ");
+  printf("My name is %s.", answer);
+
+
   return 0;
 }
