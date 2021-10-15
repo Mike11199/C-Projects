42da1bd4ba39cfe40d2ff71d10d2759ce26f71b9
Mike11199 <miwanek11199@gmail.com>
2021-10-14 22:05:20 -0700

    changed file

--- helloworld.c
+++ helloworld.c
@@ -4,6 +4,6 @@ int
 main (void)
 {
   printf ("Hello, world!  Installing the GCC compiler took forever!\n");
-  printf ("Now I can print text.\n");
+  printf ("Now I can print text in C.\n");
   return 0;
 }
