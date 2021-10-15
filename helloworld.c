1fb686cd3619ccdf2eac98a8fd4e058442fb6d9d
Mike11199 <miwanek11199@gmail.com>
2021-10-14 21:55:55 -0700

    added files

--- helloworld.c
+++ helloworld.c
@@ -3,6 +3,7 @@
 int
 main (void)
 {
-  printf ("Hello, world!  Installing C took a long time!\n");
+  printf ("Hello, world!  Installing the GCC compiler took forever!\n");
+  printf ("Now I can print text.\n");
   return 0;
 }
