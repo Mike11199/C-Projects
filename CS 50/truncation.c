cd4a9ab269023aa12403f364123435b8886d02e5
Mike11199 <miwanek11199@gmail.com>
2021-10-17 14:15:39 -0700

    edit

--- CS 50/truncation.c
+++ CS 50/truncation.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
-//#include "cs50.h"
 #include "C:\Users\miwan\Desktop\Git\C-Projects\CS 50\CS50 Header Files\cs50.h"
 
 
 int main (void)
 {
 
+    // Get numbers from user
     int x = get_int("x: ");
     int y = get_int("y: "); 
-    printf("%i\n", x+y);
-    printf("This program added the two numbers.");
+   
+   // Divide x by y.  Needs to be typecasted or else x/y will truncate and only keep one integer, not decimals
+    float z =(float)x/(float)y;
+    printf("%f\n", z);
+    printf("This program divided the two numbers.");
 
 
 }
