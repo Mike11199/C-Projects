f77cd9c038d30a6aa54858d79312243ab41295f4
Mike11199 <miwanek11199@gmail.com>
2021-10-17 16:14:08 -0700

    add

---
+++ CS 50/Lecture 1/loop.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "C:\Users\miwan\Desktop\Git\C-Projects\CS 50\CS50 Header Files\cs50.h"
+
+//prototype tells compiler that a function exists
+void meow(void);  
+
+int main (void)
+{
+    for (int i =0; i<3; i++)
+    {
+        meow();       
+        meow_v2(4);
+    }
+}
+
+//put custom functions at the bottom; convention is to keep the main program at the top of the file.
+void meow(void)
+    {
+        printf("meow\n");
+    }
+
+
+void meow_v2(int n)
+    {
+        for (int i=0; i<n; i++)
+        {
+        printf("meow2\n");
+        }
+    }
+
