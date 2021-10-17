6afde6c1de24f6da914ec9310dbc908b174c15d5
Mike11199 <miwanek11199@gmail.com>
2021-10-17 13:50:57 -0700

    edit

---
+++ CS 50/truncation.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "cs50.h"
+
+
+int main (void)
+{
+
+    int x = get_int("x: ");
+    int y = get_int("y: "); 
+    printf("%i\n", x+y);
+    printf("This program added the two numbers.");
+
+
+}
