8d4c7eff747720beef13afb828544d564fe3c6fe
Mike11199 <miwanek11199@gmail.com>
2021-10-17 16:40:44 -0700

    edit

---
+++ CS 50/Lecture 1/imprecision.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "C:\Users\miwan\Desktop\Git\C-Projects\CS 50\CS50 Header Files\cs50.h"
+
+
+int main (void)
+{
+   float x = get_float("x: ");
+   float y = get_float("y: ");
+
+    printf("%.50f\n", x/y);
+
+
+}
+
