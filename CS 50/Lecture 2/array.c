87800c8b8fb83c98f35ea1513e3b778f6237f05d
Mike11199 <miwanek11199@gmail.com>
2021-10-20 19:40:14 -0700

    edit

--- CS 50/Lecture 2/array.c
+++
@@ -1,3 +0,0 @@
-# include <stdio.h>
-# include "C:\Users\miwan\Desktop\Git\C-Projects\CS 50\CS50 Header Files\cs50.h"
-
---
+++ CS 50/Lecture 2/buggy0.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+# include "C:\Users\miwan\Desktop\Git\C-Projects\CS 50\CS50 Header Files\cs50.h"
+
+int main (void)
+{
+
+for (int i = 0; i < 10; i++)
+    {
+        printf("i is now %i\n", i);
+        printf("#\n");
+    }
+
+
+}
