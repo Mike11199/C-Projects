5e76e4062354e1f6d84c097e66deb7766d1ea4bc
Mike11199 <miwanek11199@gmail.com>
2021-10-23 17:33:00 -0700

    add

--- CS 50/Lecture 4/address_integer - String.c
+++ CS 50/Lecture 4/address_integer - String.c
@@ -3,17 +3,24 @@
 #include <string.h>
 
 
-// '&' Ampersand.  Two '&&' is the logical 'and' operator.  One '&' is the address of a variable in hexadecimal the variable is stored in. - "Figures out the address.""
-// '*' The star or asterisk operator, or dereference operator, means go to the following address. - "Goes to the address.""
-// %p is the format code for an address in hexadecimal as oppossed to %i for integer.
-
 int main (void)
 {
-    int n = 50;
-    int *p = &n;                      //'*' A pointer is a variable that contains the address of some other value. 8 bytes
-
-    printf("%i\n", *p);               //prints the integer in the address stored in p.  integer is 4 bytes
-    printf("%p\n", p);                //prints the address stored in p "hexadecimal"
-
+    char *s = "HI!";                  //pointer arithmetic to go to the next byte
+    printf("%c\n", *s);
+    printf("%c\n", *(s+1));
+    printf("%c\n", *(s+2));
+    printf("%i\n", *(s+3));
 }
+
+
+int main_previous (void)
+{
+    string s = "HI!";                 //s is a pointer to the first character of the string.  Each character of a string is back to back ending in null.
+    printf("%p\n", s);                //In C, strings exist as "char *s".  The address of a character.
+  
+    printf("%p\n", &s[0]);
+    printf("%p\n", &s[1]);
+    printf("%p\n", &s[2]);
+}
+
 
