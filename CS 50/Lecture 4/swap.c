26d3562b798b9232b3b48dac58730ed4f8507259
Mike11199 <miwanek11199@gmail.com>
2021-10-24 21:16:35 -0700

    edit

--- CS 50/Lecture 4/swap.c
+++ CS 50/Lecture 4/swap.c
@@ -2,15 +2,26 @@
 #include "C:\Users\miwan\Desktop\Git\C-Projects\CS 50\CS50 Header Files\cs50.h"
 #include <string.h>
 #include <ctype.h>
-#include <stdlib.h>                                     //access to malloc
+#include <stdlib.h>                               //access to malloc
 
+ 
+void swap (int *a, int *b);                       //prototype
 
 int main (void)
 {
+        int x = 1;
+        int y = 2;
 
-
-
-
+        printf("x is %i, y is %i\n", x, y);
+        swap(&x, &y);                             //pass the address of x and y
+        printf("x is %i, y is %i\n", x, y);
 }
 
+ 
+void swap (int *a, int *b)                         //pointers as arguments
+{
+        int tmp = *a;                             //tmp = integer "empty glass"
+        *a = *b;
+        *b = tmp;
+}
 
