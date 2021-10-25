3f0635583fcb97d6c43eabbb603493423ca0729a
Mike11199 <miwanek11199@gmail.com>
2021-10-24 19:14:00 -0700

    add

--- CS 50/Lecture 4/swap.c
+++ CS 50/Lecture 4/swap.c
@@ -7,14 +7,9 @@
 
 int main (void)
 {
-        char *s = malloc(4);
-        s[0] = 'H';
-        s[1] = 'I';
-        s[2] = '!';
-        s[3] = '\0';
-        printf("%s\n", s);
 
-        free(s);                                        //prevent memory leak
+
+
 
 }
 
