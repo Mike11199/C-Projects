96dd27cc872166de51038189f2bb5a357d944378
Mike11199 <miwanek11199@gmail.com>
2021-10-25 20:27:15 -0700

    add

---
+++ CS 50/Lecture 5/linked list.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "C:\Users\miwan\Desktop\Git\C-Projects\CS 50\CS50 Header Files\cs50.h"
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>                                      //access to malloc
+
+
+
+int main (void)                                       
+{     
+  
+
+  
+
+}
