11a34aeee24586d1cf7c521d75d711fe4a077e03
Mike11199 <miwanek11199@gmail.com>
2021-10-25 18:39:17 -0700

    add

--- CS 50/Lecture 4/phonebook.c
+++ CS 50/Lecture 4/phonebook.c
@@ -9,17 +9,6 @@
 
 int main (void)                         
 {
-        FILE *file = fopen("phonebook.csv", "a");
-        if (file == NULL)
-        {
-                return 1;
-        }
-
-        char *name = get_string("Name: ");
-        char *number = get_string("Number: ");
-
-        fprintf(file, "%s,%s\n", name, number);
-        fclose(file);
-
+ 
 }
 
