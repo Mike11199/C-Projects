f22f604e54a18bf118ee290c5f840109258fd48e
Mike11199 <miwanek11199@gmail.com>
2021-10-25 18:56:10 -0700

    add

---
+++ CS 50/Lecture 5/linked list.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "C:\Users\miwan\Desktop\Git\C-Projects\CS 50\CS50 Header Files\cs50.h"
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>                               //access to malloc
+
+
+
+//Linked lists are more efficient than arrays as you don't have to copy your original array into a new array each time.  You can grow and shrink the data.
+// are a series of nodes that are connected via pointers.  So don't have to be contiguous.
+
+
+int main (void)                                  //running time of inserting into an array is linear time O(n) or omega(1) if already empty as lower.
+{                                               //vector is an array that can be resized or shrinked.  This is NOT what arrays are in C.
+                                                
+
+}
+
