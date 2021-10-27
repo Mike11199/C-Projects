6712395e63d90c8b17dcf8f8a057de505f5f1ae4
Mike11199 <miwanek11199@gmail.com>
2021-10-26 20:44:29 -0700

    add

--- CS 50/Lecture 5/linked list.c
+++ CS 50/Lecture 5/linked list.c
@@ -5,11 +5,56 @@
 #include <stdlib.h>                                      //access to malloc
 
 
+typedef struct node
+{
+    int number;
+    struct node *next;
+}
+node;
 
 int main (void)                                       
 {     
-  
+    node *list = NULL;                  //initialize pointer to something
+    node *n = malloc(sizeof(node));     //allocate a temporary node using malloc with another room for an integer and another node
+    if (n == NULL)
+    {
+        return 1;
+    }
+    n->number = 1;                       // Allows access to the members of a structure using pointers.  Short for (*n).number and combines the dot and * operators
+    n->next = NULL;
+    list = n;
 
-  
+
+
+    n = malloc(sizeof(node));
+    if (n==NULL)
+    {
+        free(list);
+        return 1;
+    }
+    n->number =2;
+    n->next = NULL;
+    list->next = n;
+
+
+
+    n = malloc(sizeof(node));
+    if (n==NULL)
+    {
+        free(list);
+        return 1;
+    }
+    n->number =3;
+    n->next = NULL;
+    list->next->next = n;
+
+
+    for (node *tmp = list; tmp != NULL; tmp = tmp->next)
+    {
+        printf("%i\n", tmp ->number);
+    }
+
+
+
 
 }
